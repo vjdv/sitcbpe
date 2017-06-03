e27845f649d0c46da359fc386f22122d36ac006e
Jassiel <vjdv@outlook.com>
2017-06-03 00:02:11 +0000

    Se agregó una imagen
    git-svn-id: file:///var/svn/sitcbpe@4 4246a452-d782-4053-aa70-e794d9c5d3b2

--- src/net/vjdv/baz/pe/Main.java
+++ src/net/vjdv/baz/pe/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -20,6 +21,7 @@ public class Main extends Application {
         InicioController controller = loader.<InicioController>getController();
         Scene scene = new Scene(root);
         stage.setTitle("Procesos Especiales SITCB");
+        stage.getIcons().add(new Image("/net/vjdv/baz/pe/logoSIT.png"));
         stage.setScene(scene);
         stage.setOnShown(windosevent -> {
             controller.onVisible();
