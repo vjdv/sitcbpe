dd3a87913a238d8a26754305dec102d92231abb6
Jassiel <vdiaz@elektra.com.mx>
2018-12-04 12:16:20 -0600

    Highlighting improvement

--- src/net/vjdv/baz/pe/SqlPatterns.java
+++ src/net/vjdv/baz/pe/SqlPatterns.java
@@ -10,16 +10,27 @@ import org.fxmisc.richtext.model.StyleSpansBuilder;
 
 public class SqlPatterns {
 	private static final String[] KEYWORDS = new String[] { "SELECT", "INSERT", "UPDATE", "DELETE", "SET", "EXEC",
-			"FROM", "WHERE", "ORDER BY", "GROUP BY", "HAVING", "DESC", "ASC", "TOP", "INTO", "VALUES", "WITH(NOLOCK)" };
+			"FROM", "WHERE", "ORDER BY", "GROUP BY", "HAVING", "DESC", "ASC", "TOP", "INTO", "VALUES", "WITH", "NOLOCK",
+			"CREATE", "ALTER", "PROCEDURE", "FUNCTION", "PIVOT", "ON", "FOR", "XML", "PATH", "ROOT", "AS", "BEGIN",
+			"END", "TRY", "CATCH", "GO", "DECLARE", "INT", "CHAR", "VARCHAR", "DECIMAL", "DATE", "DATETIME", "BIT",
+			"OUTPUT", "ADD", "NOT", "NULL", "DEFAULT" };
 	private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + "|"
 			+ String.join("|", KEYWORDS).toLowerCase() + ")\\b";
-	private static final String[] JOINER = new String[] { "IN", "AND", "OR" };
-	private static final String JOINER_PATTERN = "\\b(" + String.join("|", JOINER) + ")\\b";
-	private static final String NUMBER_PATTERN = "[0-9]";
-	private static final String STRING_PATTERN = "'([^']|'')*'";
+	private static final String[] JOINERS = new String[] { "IN", "AND", "OR", "LEFT JOIN", "RIGHT JOIN", "INNER JOIN",
+			"BETWEEN" };
+	private static final String JOINER_PATTERN = "\\b(" + String.join("|", JOINERS) + ")\\b";
+	private static final String[] FUNCS = new String[] { "GETDATE", "ISNULL", "RTRIM", "LTRIM",
+			"SP_XML_PREPAREDOCUMENT", "SP_XML_REMOVEDOCUMENT", "OPENXML", "JSON_VALUE", "ISJSON", "JSON_QUERY",
+			"JSON_MODIFY", "CONVERT", "CAST" };
+	private static final String FUNC_PATTERN = "\\b(" + String.join("|", FUNCS) + "|"
+			+ String.join("|", FUNCS).toLowerCase() + ")\\b";
+	private static final String STRING_PATTERN = "'([^']|'')*('|\\z)";
+	private static final String COMMENT_PATTERN = "(--.*?$)|(/\\*.*?\\*/)";
 
-	public static final Pattern PATTERN = Pattern.compile("(?<KEYWORD>" + KEYWORD_PATTERN + ")" + "|(?<JOINER>"
-			+ JOINER_PATTERN + ")" + "|(?<NUMBER>" + NUMBER_PATTERN + ")" + "|(?<STRING>" + STRING_PATTERN + ")");
+	public static final Pattern PATTERN = Pattern.compile(
+			"(?<KEYWORD>" + KEYWORD_PATTERN + ")" + "|(?<JOINER>" + JOINER_PATTERN + ")" + "|(?<FUNC>" + FUNC_PATTERN
+					+ ")" + "|(?<STRING>" + STRING_PATTERN + ")" + "|(?<COMMENT>" + COMMENT_PATTERN + ")",
+			Pattern.MULTILINE | Pattern.DOTALL);
 
 	public static StyleSpans<Collection<String>> computeHighlight(String text) {
 		Matcher matcher = PATTERN.matcher(text);
@@ -28,8 +39,9 @@ public class SqlPatterns {
 		while (matcher.find()) {
 			String styleClass = matcher.group("KEYWORD") != null ? "keyword"
 					: matcher.group("JOINER") != null ? "joiner"
-							: matcher.group("NUMBER") != null ? "number"
-									: matcher.group("STRING") != null ? "string" : null; /* never happens */
+							: matcher.group("FUNC") != null ? "func"
+									: matcher.group("STRING") != null ? "string"
+											: matcher.group("COMMENT") != null ? "comment" : null;
 			assert styleClass != null;
 			spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
 			spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
