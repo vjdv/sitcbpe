0a664c65e48459330c021bc06046c52727c89caf
Jassiel <vdiaz@elektra.com.mx>
2019-10-11 11:49:22 -0500

    quita funciones repetidas

--- src/net/vjdv/baz/pe/ResultSetWindow.java
+++ src/net/vjdv/baz/pe/ResultSetWindow.java
@@ -1,52 +1,33 @@
 package net.vjdv.baz.pe;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
-import java.text.Normalizer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TablePosition;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
+import javafx.scene.control.*;
+import javafx.scene.input.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import net.vjdv.baz.pe.Result.ResultPage;
 
+import java.awt.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.text.Normalizer;
+import java.util.List;
+import java.util.*;
+
 /**
- *
  * @author B187926
  */
 public class ResultSetWindow {
@@ -69,14 +50,12 @@ public class ResultSetWindow {
         MenuItem mitem6 = new MenuItem("para UPDATE");
         MenuItem mitem8 = new MenuItem("para WHERE IN");
         MenuItem mitem4 = new MenuItem("Expandir");
-        MenuItem mitem12 = new MenuItem("Ver en Notepad++");
-        MenuItem mitem3 = new MenuItem("Guardar como .csv");
-        MenuItem mitem7 = new MenuItem("Guardar como .txt");
+        MenuItem mitem12 = new MenuItem("Exportar");
         Menu mitem9 = new Menu("Copiar como sql...");
         MenuItem mitem10 = new MenuItem("Ir a columna");
         MenuItem mitem11 = new MenuItem("Etiquetar ventana");
         mitem9.getItems().addAll(mitem5, mitem6, mitem8);
-        menu.getItems().addAll(mitem1, mitem2, mitem9, new SeparatorMenuItem(), mitem4, mitem12, new SeparatorMenuItem(), mitem11, mitem10, new SeparatorMenuItem(), mitem3, mitem7);
+        menu.getItems().addAll(mitem1, mitem2, mitem9, new SeparatorMenuItem(), mitem4, mitem12, new SeparatorMenuItem(), mitem11, mitem10);
         tabla.setContextMenu(menu);
         // Acciones menú
         EventHandler<ActionEvent> listener = (ActionEvent event) -> {
@@ -104,7 +83,7 @@ public class ResultSetWindow {
             for (TablePosition position : positionList) {
                 int row = position.getRow();
                 int col = position.getColumn();
-                Object cell = (Object) tabla.getColumns().get(col).getCellData(row);
+                Object cell = tabla.getColumns().get(col).getCellData(row);
                 if (cell == null) {
                     cell = "";
                 }
@@ -126,22 +105,6 @@ public class ResultSetWindow {
         };
         mitem1.setOnAction(listener);
         mitem2.setOnAction(listener);
-        mitem3.setOnAction(actionEvent -> {
-            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
-                    "Archivo de valores separados por comas (*.csv)", "*.csv");
-            FileChooser chooser = new FileChooser();
-            chooser.getExtensionFilters().add(extFilter);
-            File file = chooser.showSaveDialog(null);
-            if (file != null) {
-                try (PrintWriter out = new PrintWriter(file)) {
-                    out.print(resultset.toString());
-                } catch (FileNotFoundException ex) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setContentText("Error al guardar el CSV: " + ex.toString());
-                    alert.showAndWait();
-                }
-            }
-        });
         mitem4.setOnAction(actionEvent -> {
             ObservableList<TablePosition> positionList = tabla.getSelectionModel().getSelectedCells();
             for (TablePosition position : positionList) {
@@ -186,7 +149,7 @@ public class ResultSetWindow {
             for (TablePosition position : positionList) {
                 int row = position.getRow();
                 int col = position.getColumn();
-                Object value = (Object) tabla.getColumns().get(col).getCellData(row);
+                Object value = tabla.getColumns().get(col).getCellData(row);
                 if (value instanceof String) {
                     value = ((String) value).replaceAll("'", "''");
                     value = "'" + value + "'";
@@ -274,23 +237,6 @@ public class ResultSetWindow {
             clipboardContent.putString(clipboardString.toString());
             Clipboard.getSystemClipboard().setContent(clipboardContent);
         });
-        // Guardar como .txt
-        mitem7.setOnAction(actionEvent -> {
-            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Archivo de texto (*.txt)",
-                    "*.txt");
-            FileChooser chooser = new FileChooser();
-            chooser.getExtensionFilters().add(extFilter);
-            File file = chooser.showSaveDialog(null);
-            if (file != null) {
-                try (PrintWriter out = new PrintWriter(file)) {
-                    out.print(resultset);
-                } catch (FileNotFoundException ex) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setContentText("Error al guardar el CSV: " + ex.toString());
-                    alert.showAndWait();
-                }
-            }
-        });
         // Buscar columna
         mitem10.setOnAction(actionEvent -> {
             TextInputDialog filteringDialog = new TextInputDialog();
@@ -324,15 +270,14 @@ public class ResultSetWindow {
             }).forEachOrdered((str) -> {
                 String ext = str.startsWith("<") ? ".xml" : ".txt";
                 try {
-                    File tmp = File.createTempFile("sitcbpe", ext);
+                    File tmp = File.createTempFile("salidacol", ext);
                     tmp.deleteOnExit();
                     try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(tmp), StandardCharsets.UTF_8)) {
                         writer.write(str);
                     }
-                    String[] args = {"nppp", tmp.getAbsolutePath()};
-                    new ProcessBuilder(args).start();
+                    Desktop.getDesktop().open(tmp);
                 } catch (IOException ex) {
-                    Alert alertDialog = new Alert(Alert.AlertType.INFORMATION);
+                    Alert alertDialog = new Alert(Alert.AlertType.WARNING);
                     alertDialog.setContentText(ex.getMessage());
                     alertDialog.setHeaderText(ex.getClass().getName());
                     alertDialog.setTitle("Error");
@@ -346,9 +291,9 @@ public class ResultSetWindow {
         mitem4.setAccelerator(new KeyCodeCombination(KeyCode.P, KeyCombination.CONTROL_DOWN));
         mitem5.setAccelerator(new KeyCodeCombination(KeyCode.I, KeyCombination.CONTROL_DOWN));
         mitem6.setAccelerator(new KeyCodeCombination(KeyCode.U, KeyCombination.CONTROL_DOWN));
-        mitem7.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN));
         mitem8.setAccelerator(new KeyCodeCombination(KeyCode.W, KeyCombination.CONTROL_DOWN));
         mitem10.setAccelerator(new KeyCodeCombination(KeyCode.G, KeyCombination.CONTROL_DOWN));
+        mitem12.setAccelerator(new KeyCodeCombination(KeyCode.E, KeyCombination.CONTROL_DOWN));
         // Interpretado de resultado
         for (int i = 0; i < resultset.columns.length; i++) {
             final int ifinal = i;
