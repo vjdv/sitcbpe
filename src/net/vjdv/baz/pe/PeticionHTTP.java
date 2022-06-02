b3c0115304252e1a6d9df8f92dd68e754da8cadf
Jassiel <vdiaz@elektra.com.mx>
2022-06-02 13:19:22 -0500

    reformat

--- src/net/vjdv/baz/pe/PeticionHTTP.java
+++ src/net/vjdv/baz/pe/PeticionHTTP.java
@@ -27,48 +27,48 @@ import javafx.concurrent.Task;
  */
 public class PeticionHTTP extends Task<Result> {
 
-	private final String query;
-	private final URL url;
-	private final ObjectMapper objectMapper = new ObjectMapper();
+  private final String query;
+  private final URL url;
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
-	public PeticionHTTP(URL url, String consulta) {
-		query = consulta;
-		this.url = url;
-	}
+  public PeticionHTTP(URL url, String consulta) {
+    query = consulta;
+    this.url = url;
+  }
 
-	public Result enviarConsulta() {
-		Result result = new Result();
-		RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(10000).build();
-		try (CloseableHttpClient httpclient = HttpClients.custom().build()) {
-			// Creando parámetros a enviar
-			Token token = new Token("SITCB3");
-			HttpPost httppost = new HttpPost(url.toString());
-			httppost.setConfig(requestConfig);
-			// Enviando par�metros
-			List<NameValuePair> params = new ArrayList<>(3);
-			params.add(new BasicNameValuePair("tran", query));
-			params.add(new BasicNameValuePair("timestamp", "" + token.getTimestamp()));
-			params.add(new BasicNameValuePair("token", token.get()));
-			httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-			// Leyendo respuesta
-			HttpResponse response = httpclient.execute(httppost);
-			HttpEntity entity = response.getEntity();
-			updateMessage("Leyendo respuesta");
-			String json = EntityUtils.toString(entity);
-			updateMessage("Parseando respuesta");
-			result = objectMapper.readValue(json, Result.class);
-		} catch (ConnectException ex) {
-			result.error = "No se pudo establecer una conexi\u00f3n con el servidor";
-		} catch (IOException ex) {
-			result.error = ex.getMessage();
-		}
-		return result;
-	}
+  public Result enviarConsulta() {
+    Result result = new Result();
+    RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(10000).build();
+    try (CloseableHttpClient httpclient = HttpClients.custom().build()) {
+      // Creando parámetros a enviar
+      Token token = new Token("SITCB3");
+      HttpPost httppost = new HttpPost(url.toString());
+      httppost.setConfig(requestConfig);
+      // Enviando par�metros
+      List<NameValuePair> params = new ArrayList<>(3);
+      params.add(new BasicNameValuePair("tran", query));
+      params.add(new BasicNameValuePair("timestamp", "" + token.getTimestamp()));
+      params.add(new BasicNameValuePair("token", token.get()));
+      httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+      // Leyendo respuesta
+      HttpResponse response = httpclient.execute(httppost);
+      HttpEntity entity = response.getEntity();
+      updateMessage("Leyendo respuesta");
+      String json = EntityUtils.toString(entity);
+      updateMessage("Parseando respuesta");
+      result = objectMapper.readValue(json, Result.class);
+    } catch (ConnectException ex) {
+      result.error = "No se pudo establecer una conexi\u00f3n con el servidor";
+    } catch (IOException ex) {
+      result.error = ex.getMessage();
+    }
+    return result;
+  }
 
-	@Override
-	protected Result call() throws Exception {
-		updateMessage("Enviando consulta");
-		return enviarConsulta();
-	}
+  @Override
+  protected Result call() throws Exception {
+    updateMessage("Enviando consulta");
+    return enviarConsulta();
+  }
 
 }
